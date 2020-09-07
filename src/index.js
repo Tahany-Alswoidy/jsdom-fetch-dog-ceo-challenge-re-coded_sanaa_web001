eed791a2e64f89a6d75e65d809dffc7f06be16bd
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-07 17:38:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 console.log('%c HI', 'color: firebrick')
-document.addEventListener('DOMContenetLoaded',function(){
+document.addEventListener('DOMContenetLoaded',function(e){
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
   fetch(imgUrl).then((response)=>response.json()).then((data)=>{
     data.forEach((image)=>
