30e3e3f5f47227ded23abbfa4e56f416af7c3ed5
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-07 17:36:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContenetLoaded',function(){
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
   fetch(imgUrl).then((response)=>response.json()).then((data)=>{
     data.forEach((image)=>
-    {
+    { console.log(image);
       document.getElementById("dog-image-container").appendChild(document.createElement("img").setAttribute("src",image));
       
     })
